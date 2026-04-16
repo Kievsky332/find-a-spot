7219010bf707959b014cf73ba34874915aad066a
Kievsky332 <falileevry@gmail.com>
2026-04-16 22:17:16 +0300

    update

--- script.js
+++ script.js
@@ -9,19 +9,16 @@ document.addEventListener('DOMContentLoaded', function() {
     const sW = window.innerWidth; //Ширина
     const sH = window.innerHeight; //высота
 
-    win.style.display = "none";
+   
 
-    let x = Math.floor(Math.random() * sW+1); // рандомая  ширина
-    let y = Math.floor(Math.random() * sH+1); // рандомная высота
+    let x = Math.floor(Math.random() * sW-99); // рандомая  ширина
+    let y = Math.floor(Math.random() * sH-99); // рандомная высота
 
 
-    bam.style.width = "100px";
-    bam.style.height = "100px";
+
     
-    bam.style.borderRadius = "100%";
 
 
-    bam.style.position = "absolute";
 
     //смещаем
     bam.style.top = y+"px"; // по y 
@@ -34,7 +31,6 @@ document.addEventListener('DOMContentLoaded', function() {
     document.addEventListener('mousemove', (e) => { //проверяем что точка найдена
         if (e.clientX > x && e.clientX < x + 100 &&e.clientY > y && e.clientY < y + 100){ 
             var audio = new Audio('pip.mp3');
-            audio.play(); 
             win.style.display = "block";   
             bam.style.background="#ff0000";
             stop_count();
